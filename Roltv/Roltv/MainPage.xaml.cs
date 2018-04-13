8e5c3f00b083ad9afc851e42ba4630c9e84a9fdd
Harold Pulcher <pulcher@pulcher.biz>
2018-04-13 09:17:23 -0500

    got the rotation working so that is mirrors

--- Roltv/Roltv/MainPage.xaml.cs
+++ Roltv/Roltv/MainPage.xaml.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
 using System.Threading.Tasks;
 using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Media;
 using Windows.Media.Capture;
+using Windows.Media.FaceAnalysis;
+using Windows.Media.MediaProperties;
+using Windows.System.Threading;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -21,7 +27,13 @@ namespace Roltv
 {
     public sealed partial class MainPage : Page
     {
+        private const int framesPerSecond = 10; // current limit on the plan for processing images
+
         private MediaCapture mediaCapture;
+        private ThreadPoolTimer frameProcessingTimer;
+        private FaceTracker faceTracker;
+        private SemaphoreSlim frameProcessingSimaphore = new SemaphoreSlim(1);
+        private VideoEncodingProperties videoProperties;
 
         public MainPage()
         {
@@ -41,12 +53,24 @@ namespace Roltv
                 {
                     mediaCapture = new MediaCapture();
                     await mediaCapture.InitializeAsync();
+                    await SetCaptureResolution();
 
                     mediaCapture.Failed += new MediaCaptureFailedEventHandler(MediaCaptureFailed);
-
+                     
                     previewElement.Source = mediaCapture;
+                    previewElement.FlowDirection = FlowDirection.RightToLeft;
+
+                    videoProperties = mediaCapture.VideoDeviceController
+                        .GetMediaStreamProperties(MediaStreamType.VideoPreview) as VideoEncodingProperties;
+
+                    //mediaCapture.SetPreviewRotation(VideoRotation.Clockwise180Degrees);
+
                     await mediaCapture.StartPreviewAsync();
 
+                    var test = 1000 / framesPerSecond;
+
+                    var timerInterval = TimeSpan.FromMilliseconds(1000 / framesPerSecond); // gets us seconds/frame
+                    frameProcessingTimer = ThreadPoolTimer.CreatePeriodicTimer(new TimerElapsedHandler(ProcessVideoFrame), timerInterval);
 
                     UpdateStatus("Camera found.  Initialized for video recording", 1000);
                 }
@@ -54,6 +78,55 @@ namespace Roltv
             catch (Exception ex)
             {
                 UpdateStatus($"Unable to initialize camera: {ex.Message}");
+            }
+        }
+
+        private async Task SetCaptureResolution()
+        {
+            var availableResolutions = mediaCapture.VideoDeviceController.GetAvailableMediaStreamProperties(MediaStreamType.VideoPreview)
+                .Cast<VideoEncodingProperties>().OrderByDescending(v => v.Width * v.Height * (v.FrameRate.Numerator / v.FrameRate.Denominator));
+
+            uint maxHeight = 720;
+            var videoEncodingSetting = availableResolutions.FirstOrDefault(v => v.Height <= maxHeight);
+
+            if(videoEncodingSetting != null)
+            {
+                await mediaCapture.VideoDeviceController.SetMediaStreamPropertiesAsync(MediaStreamType.VideoPreview, videoEncodingSetting);
+            }
+        }
+
+        private async void ProcessVideoFrame(ThreadPoolTimer timer)
+        {
+            if (!frameProcessingSimaphore.Wait(0))
+            {
+                // We are already doing something
+                return;
+            }
+
+            try
+            {
+                IEnumerable<DetectedFace> faces = null;
+
+                const BitmapPixelFormat inputPixelFormat = BitmapPixelFormat.Nv12;
+
+                using (var previewFrame = new VideoFrame(inputPixelFormat, (int)videoProperties.Width, (int)videoProperties.Height))
+                {
+                    var test = "";
+
+                    await mediaCapture.GetPreviewFrameAsync(previewFrame);
+
+                    if (FaceTracker.IsBitmapPixelFormatSupported(previewFrame.SoftwareBitmap.BitmapPixelFormat))
+                    {
+                        faces = await faceTracker.ProcessNextFrameAsync(previewFrame);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                frameProcessingSimaphore.Release();
             }
         }
 

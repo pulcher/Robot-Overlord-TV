68fdf46f098758047f7a9ccbbbfb9303f32b129e
Harold Pulcher <pulcher@pulcher.biz>
2018-07-15 13:55:11 -0500

    added in some helper classes

---
+++ Roltv/ServiceHelpers/Group.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Face.Contract;
+
+namespace ServiceHelpers
+{
+    public class Group
+    {
+        public static PersonGroup HeroVillanGroupExists(PersonGroup[] groups)
+        {
+            var foundGroup = groups.Where(x => x.Name == GroupData.HeroVillanGroupName);
+
+            return foundGroup.FirstOrDefault();
+        }
+
+        public static bool IsHero (Person person)
+        {
+            return GroupData.Heros.Contains(person.Name);
+        }
+    }
+}
---
+++ Roltv/ServiceHelpers/GroupData.cs
@@ -0,0 +1,22 @@
+﻿namespace ServiceHelpers
+{
+    class GroupData
+    {
+        public static string HeroVillanGroupName = "HeroOrVillian";
+
+        public static string[] Heros =
+        {
+            "Superman",
+            "Ironman",
+            "Wonder Woman"
+        };
+
+        public static string[] Villans =
+        {
+            "Darksied",
+            "Blank Manta",
+            "Ultron",
+            "Thanos"
+        };
+    }
+}
